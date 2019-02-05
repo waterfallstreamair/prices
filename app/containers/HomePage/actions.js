6384321ca77911f8f44f65a4025d60528ab4fac9
Alex Waterfall <waterfallstreamair@gmail.com>
2019-02-05 17:02:59 +0000

    saga updated

--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -4,46 +4,38 @@ export const getAgenciesRequest = () => ({
   type: constants.TYPE_AGENCIES_REQUEST,
 });
 
-export const setAgencies = ({ agencies }) => {
-  return {
-    type: constants.TYPE_AGENCIES_SUCCESS,
-    agencies,
-  };
-};
+export const setAgencies = ({ agencies }) => ({
+  type: constants.TYPE_AGENCIES_SUCCESS,
+  agencies,
+});
 
 export const getCategoriesRequest = ({ agencyId }) => ({
   type: constants.TYPE_CATEGORIES_REQUEST,
-  agencyId
+  agencyId,
 });
 
-export const setCategories = ({ categories }) => {
-  return {
-    type: constants.TYPE_CATEGORIES_SUCCESS,
-    categories,
-  };
-};
+export const setCategories = ({ categories }) => ({
+  type: constants.TYPE_CATEGORIES_SUCCESS,
+  categories,
+});
 
 export const getPricesRequest = ({ agencyId, categoryId }) => ({
   type: constants.TYPE_PRICES_REQUEST,
   agencyId,
-  categoryId
+  categoryId,
 });
 
-export const setPrices = ({ prices }) => {
-  return {
-    type: constants.TYPE_PRICES_SUCCESS,
-    prices,
-  };
-};
+export const setPrices = ({ prices }) => ({
+  type: constants.TYPE_PRICES_SUCCESS,
+  prices,
+});
 
 export const getFilteredRequest = ({ isValid }) => ({
   type: constants.TYPE_FILTER_REQUEST,
   isValid,
 });
 
-export const setFiltered = ({ filtered }) => {
-  return {
-    type: constants.TYPE_FILTER_SUCCESS,
-    filtered,
-  };
-};
+export const setFiltered = ({ filtered }) => ({
+  type: constants.TYPE_FILTER_SUCCESS,
+  filtered,
+});
--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest, all /* select */ } from 'redux-saga/effects';
+import { call, put, takeLatest, all, select } from 'redux-saga/effects';
 import request from 'utils/request';
 import {
   CONST_URL_BASE,
@@ -6,16 +6,42 @@ import {
   CONST_URL_CATEGORIES,
   CONST_URL_PRICES,
   TYPE_PRICES_REQUEST,
+  TYPE_AGENCIES_REQUEST,
+  TYPE_CATEGORIES_REQUEST,
+  TYPE_FILTER_REQUEST,
 } from './constants';
 import * as actions from './actions';
-// import { makeSelectPrices } from './selectors';
+import { makeSelectPrices } from './selectors';
 
-export function* getPrices() {
-  const agencyId = 1;
-  const categoryId = 1;
+export function* getAgencies() {
   const urlAgencies = `${CONST_URL_BASE}${CONST_URL_AGENCIES}`;
-  const urlCategories = `${urlAgencies}/${agencyId}/${CONST_URL_CATEGORIES}`;
-  const urlPrices = `${urlCategories}/${categoryId}/${CONST_URL_PRICES}`;
+  console.log({ urlAgencies });
+  try {
+    const agencies = yield call(request, urlAgencies);
+    yield put(actions.setAgencies({ agencies }));
+  } catch (e) {
+    // yield put(actions.showInfo({ e }));
+  }
+}
+
+export function* getCategories({ agencyId }) {
+  const urlAgencies = `${CONST_URL_BASE}${CONST_URL_AGENCIES}`;
+  const urlCategories = `${urlAgencies}/${agencyId ||
+    1}/${CONST_URL_CATEGORIES}`;
+  console.log({ urlCategories });
+  try {
+    const agencies = yield call(request, urlAgencies);
+    yield put(actions.setAgencies({ agencies }));
+  } catch (e) {
+    // yield put(actions.showInfo({ e }));
+  }
+}
+
+export function* getPrices({ agencyId, categoryId }) {
+  const urlAgencies = `${CONST_URL_BASE}${CONST_URL_AGENCIES}`;
+  const urlCategories = `${urlAgencies}/${agencyId ||
+    1}/${CONST_URL_CATEGORIES}`;
+  const urlPrices = `${urlCategories}/${categoryId || 1}/${CONST_URL_PRICES}`;
   console.log({ urlPrices });
   try {
     const prices = yield call(request, urlPrices);
@@ -24,26 +50,25 @@ export function* getPrices() {
     // yield put(actions.showInfo({ e }));
   }
 }
-/*
-export function* setPricesFilter(action) {
-  const { isValid } = action;
+
+export function* getFiltered({ isValid }) {
   try {
     const prices = yield select(makeSelectPrices());
-    const filtered = prices.filter(e => e.isValid)
-    yield put(actions.setPricesFilter({ filtered }));
+    const filtered = prices.filter(e => e.isValidated === isValid);
+    yield put(actions.setFilter({ filtered }));
   } catch (e) {
     // yield put(actions.showInfo({ e }));
   }
 }
-*/
+
 /**
  * Root saga manages watcher lifecycle
  */
 export default function* pricesData() {
   yield all([
-    // takeLatest(TYPE_AGENCIES_REQUEST, getAgencies),
-    // takeLatest(TYPE_CATEGORIES_REQUEST, getCategories),
+    takeLatest(TYPE_AGENCIES_REQUEST, getAgencies),
+    takeLatest(TYPE_CATEGORIES_REQUEST, getCategories),
     takeLatest(TYPE_PRICES_REQUEST, getPrices),
-    // takeLatest(TYPE_PRICES_FILTER_REQUEST, setPricesFilter),
+    takeLatest(TYPE_FILTER_REQUEST, getFiltered),
   ]);
 }
